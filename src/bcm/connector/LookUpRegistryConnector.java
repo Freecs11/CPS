d3e3d785a6fc65acc00ac7cde41b0bd714c0c358
rachid <55999910+Freecs11@users.noreply.github.com>
2024-02-26 14:47:55 +0100

    lookUpRegistry Connector

---
+++ src/bcm/connector/LookUpRegistryConnector.java
@@ -0,0 +1,23 @@
+package bcm.connector;
+
+import java.util.Set;
+
+import fr.sorbonne_u.components.connectors.AbstractConnector;
+import fr.sorbonne_u.cps.sensor_network.interfaces.ConnectionInfoI;
+import fr.sorbonne_u.cps.sensor_network.interfaces.GeographicalZoneI;
+import fr.sorbonne_u.cps.sensor_network.registry.interfaces.LookupCI;
+
+public class LookUpRegistryConnector extends AbstractConnector
+        implements LookupCI {
+
+    @Override
+    public ConnectionInfoI findByIdentifier(String sensorNodeId) throws Exception {
+        return ((LookupCI) this.offering).findByIdentifier(sensorNodeId);
+    }
+
+    @Override
+    public Set<ConnectionInfoI> findByZone(GeographicalZoneI z) throws Exception {
+        return ((LookupCI) this.offering).findByZone(z);
+    }
+
+}
